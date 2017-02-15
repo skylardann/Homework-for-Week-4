b2f4760b4638f467e137da8b218ceaa2e675219d
skylardann <skylardann@cca.edu>
2017-02-15 02:42:07 -0800

    Create sketch-8.js

---
+++ sketch-8.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(0);
+
+  stroke(225);
+
+  for (var i = 20; i <= 480; i += 10) {
+    line(i, 20, 20, i);
+  }
+  for (var j = 30; j < 490; j += 10) {
+    line(480, j, j, 480);
+  }
+}
